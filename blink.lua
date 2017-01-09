13002a8d8a1526083b6522f74e89e650d62e124b
Hwulex <hwulex@gmail.com>
2017-01-08 23:19:29 -0800

    Initital commit with basic scripts

---
+++ blink.lua
@@ -0,0 +1,18 @@
+-- Blink using timer alarm --
+timerId = 0
+dly = 1000
+
+-- D4 = ESP
+-- D0 = NodeMCU
+-- D2 = LED
+ledPin = 4
+
+-- set mode to output
+gpio.mode(ledPin,gpio.OUTPUT)
+ledState = 0
+-- timer loop
+tmr.alarm( timerId, dly, 1, function()
+  ledState = 1 - ledState;
+  -- write state to D4
+  gpio.write(ledPin, ledState)
+end)
